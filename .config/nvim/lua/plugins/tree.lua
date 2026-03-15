4e810ca6ad237a639df20d620759df69e65e6fb6
Nicolai Kallis <nicolai.kallis@gmx.de>
2026-03-15 04:01:25 +0100

    implements tree file operations

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -6,6 +6,28 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup({
+      view = {
+        width = 30,
+      },
+      renderer = {
+        icons = {
+          show = {
+            git = true,
+          },
+        },
+      },
+      actions = {
+        open_file = {
+          quit_on_open = false,
+        },
+      },
+      update_focused_file = {
+        enable = true,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true })
+    vim.keymap.set("n", "<leader>tf", ":NvimTreeFindFile<CR>", { silent = true })
   end,
 }
